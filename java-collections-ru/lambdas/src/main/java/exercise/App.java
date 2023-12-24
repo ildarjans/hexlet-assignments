2a1822fb166095f1a4a5feab9f4e3d99fbd4c07f
@hexlet/cli <support@hexlet.io>
2023-12-24 20:36:44 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -3,5 +3,25 @@ package exercise;
 import java.util.Arrays;
 
 // BEGIN
+class App {
+    public static void main(String[] args) {}
+
+    public static String[][] enlargeArrayImage(String[][] arr) {
+        int rowsLen = arr.length * 2;
+        int columnsLen = arr[0].length * 2;
+        String[][] result = new String[rowsLen][columnsLen];
+
+        for (int row = 0; row < rowsLen; row++) {
+            int i = (int)Math.floor(row / 2);
+            for (int col = 0; col < columnsLen; col++) {
+                int j = (int)Math.floor(col / 2);
+
+                result[row][col] = arr[i][j];
+            }
+        }
+
+        return result;
+    }
+}
 
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -1,8 +1,40 @@
 package exercise;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
+class AppTest {
 
+    @Test
+    void testEnlargeArrayImage() {
+        // BEGIN
+
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
+
+        String[][] expected = {
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+            {"*", "*", " ", " ", " ", " ", "*", "*"},
+            {"*", "*", " ", " ", " ", " ", "*", "*"},
+            {"*", "*", " ", " ", " ", " ", "*", "*"},
+            {"*", "*", " ", " " ," ", " ", "*", "*"},
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+        };
+
+        String[][] result = App.enlargeArrayImage(image);
+        assertThat(Arrays.deepToString(result)).isEqualTo(Arrays.deepToString(expected));
+        // END
+    }
+}
 // END
